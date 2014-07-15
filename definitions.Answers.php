c61a17e08ecd7b6828f4f891609d1847fc843865
srbz <srbz@hotmail.de>
2014-07-15 22:03:31 +0200

    Update definitions.Answers.php
    Added another translation for "See post in context" -- thinking its the quite closesest translations.

--- definitions.Answers.php
+++ definitions.Answers.php
@@ -8,5 +8,5 @@ $definitions["Answer"] = "Antwort";
 $definitions["Answered"] = "Beantwortet";
 $definitions["This post answered my question"] = "Dieser Beitrag beantwortet meine Frage";
 $definitions["Answered by %s"] = "Antwort von %s";
-$definitions["See post in context"] = "See post in context";
+$definitions["See post in context"] = "Im Kontext betrachten";
 $definitions["Remove answer"] = "Antwort entfernen";
