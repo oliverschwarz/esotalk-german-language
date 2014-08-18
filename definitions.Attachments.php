f1bc209bbe3063cda60d46758e6d08f0bead0536
Oliver Schwarz <Oliver Schwarz@192.168.2.101>
2014-08-18 10:36:31 +0200

    Localisation for debug texts.

--- definitions.Attachments.php
+++ definitions.Attachments.php
@@ -2,8 +2,8 @@
 // Copyright 2013 Toby Zerner, Simon Zerner
 // This file is part of esoTalk. Please see the included license file for usage information.
 
-// English Definitions for the Answers plugin.
+// German Definitions for the Answers plugin.
 
-$definitions["Attach a file"] = "Attach a file";
-$definitions["Drop files to upload"] = "Drop files to upload";
-$definitions["message.attachmentNotFound"] = "For some reason this attachment cannot be viewed. It may not exist, or you may not have permission to view it.";
+$definitions["Attach a file"] = "Datei anh&auml;ngen";
+$definitions["Drop files to upload"] = "Dateien zum Hochladen hier hinziehen";
+$definitions["message.attachmentNotFound"] = "Der Anhang kann nicht angezeigt werden. Er existiert eventuell nicht oder Du hast nicht die Rechte, ihn anzusehen.";
