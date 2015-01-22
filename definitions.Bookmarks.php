e4a56fdfa157088c966a17424cd0d7bb6ea15fc9
Michael Roitzsch <reactorcontrol@icloud.com>
2015-01-23 00:22:03 +0100

    add translation for Bookmarks plugin

---
+++ definitions.Bookmarks.php
@@ -0,0 +1,8 @@
+<?php
+
+// German definitions for the Bookmarks plugin.
+
+$definitions["Bookmark"] = "Lesezeichen merken";
+$definitions["Unbookmark"] = "Lesezeichen entfernen";
+$definitions["gambit.bookmarked"] = "Lesezeichen";
+$definitions["label.bookmarked"] = "Lesezeichen";
